641106593271670b996bbb8bfeb493116558b502
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-10-04 19:19:27 -0400

    index update

---
+++ assets/js/question.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: 'Commonly used data type DO NOT include.',
+        choice: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts',
+    },
+    {
+        title: 'The condition in an if/else statement is enclosed within.',
+        choice: ['quotes', 'curly brackets', 'parenthese', 'sqaure brackets'],
+        answer: 'parenthese',
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store ______.',
+        choice: [
+            'numbers and strings',
+            'other arrays',
+            'booleans',
+            'all the above',
+        ],
+        answer: 'all of the above',
+    },
+    {
+        title: 'Strings values must be enclosed within _____ when being assigned to variables.',
+        choice: ['curly backets', 'square brackets', 'quotes', 'none of the above'],
+        answer: 'quotes',
+    }
+]
